df272b68ba4262f478540e7f1cd5f984f4c52745
luisagoncalves <luisaolg20@gmail.com>
2025-10-12 20:38:38 -0300

    add android config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Futdelas',
+  appId: 'com.luisag.futdelas',
+  appName: 'FutDelas',
   webDir: 'dist'
 };
 
